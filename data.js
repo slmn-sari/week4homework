faafdb88e74400f61ae9f959e04389460de55b7e
slmn-sari <78564743+slmn-sari@users.noreply.github.com>
2021-03-04 16:37:53 +0200

    new

---
+++ data.js
@@ -0,0 +1,23 @@
+  
+/**
+ * @param {number} id
+ * @param {string} marka
+ * @param {string} model
+ * @param {string} renk
+ * @param {string} Kullanim
+ * @param {number} modelyili
+ */
+const cars = [
+    { id: 1, marka: 'Asus', model: 'AAA1000', renk: 'Siyah', Kullanim: 'Notebook' , modelyili: 2020 },
+    { id: 2, marka: 'Lenova', model: 'CCC1000', renk: 'gri', Kullanim: 'Notebook' , modelyili: 2018  },
+    { id: 3, marka: 'Fujitsu', model: 'DDD1000', renk: 'siyah', Kullanim: 'Desktop' , modelyili: 2014  },
+    { id: 4, marka: 'Acer', model: 'RRR1000', renk: 'kirmizi', Kullanim: 'Desktop' , modelyili: 2020  },
+    { id: 5, marka: 'Hp', model: 'HHH1000', renk: 'Beyaz', Kullanim: 'Notebook' , modelyili: 2015  },
+    { id: 6, marka: 'Samsung', model: 'SSS1000', renk: 'kirmizi', Kullanim: 'Desktop' , modelyili: 2021  },
+];
+
+module.exports = computer;
+
+
+
+
